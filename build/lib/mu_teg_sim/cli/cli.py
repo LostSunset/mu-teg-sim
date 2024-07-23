073dd22554e174465be6d32210c0732bd1d4773b
TheBerriesLab <76480495+dabeLab@users.noreply.github.com>
2024-07-23 18:11:31 +0200

    Init

--- build/lib/mu_teg_sim/cli/cli.py
+++
@@ -1,4 +0,0 @@
-def main():
-    from mu_teg_sim.gui.main_frame import MainFrame
-    app = MainFrame()
-    app.mainloop()
--- build/lib/mu_teg_sim/main.py
+++
@@ -1,5 +0,0 @@
-from mu_teg_sim.gui.main_frame import MainFrame
-
-if __name__ == "__main__":
-    app = MainFrame()
-    app.mainloop()
